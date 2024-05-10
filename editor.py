6b901c5dac487d7b8959ab4b1bff4e1302abe7c4
vincent-el <vince198299@gmail.com>
2024-05-11 01:12:48 +0900

    add soundtrack

--- editor.py
+++ editor.py
@@ -1,5 +1,7 @@
 import os
-from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip, concatenate_videoclips
+from moviepy.audio.fx.volumex import volumex
+from moviepy.audio.fx.audio_fadeout import audio_fadeout
 import re
 
 def create_movie_clip(audio_folder, video_folder, output_file):
@@ -16,7 +18,6 @@ def create_movie_clip(audio_folder, video_folder, output_file):
         raise ValueError("The number of audio and video files does not match.")
 
     clips = []
-
     # Create clips by combining corresponding audio and video files
     for audio_file, video_file in zip(audio_files, video_files):
         video_path = os.path.join(video_folder, video_file)
@@ -25,13 +26,16 @@ def create_movie_clip(audio_folder, video_folder, output_file):
         video_clip = VideoFileClip(video_path)
         audio_clip = AudioFileClip(audio_path)
 
-        # Set the audio of the video clip
         video_clip = video_clip.set_audio(audio_clip)
 
         clips.append(video_clip)
-
-    # Concatenate all clips into one final clip
+    
     final_clip = concatenate_videoclips(clips, method="compose")
-
-    # Write the result to a file
+    
+    main_theme_audio = AudioFileClip("./assets/audio/main_theme.mp3")
+    main_theme_audio = main_theme_audio.set_duration(final_clip.duration)
+    main_theme_audio = volumex(main_theme_audio, 0.1)
+    main_theme_audio = audio_fadeout(main_theme_audio, 10)
+    final_clip = final_clip.set_audio(CompositeAudioClip([final_clip.audio, main_theme_audio]))
+    
     final_clip.write_videofile(output_file, codec="libx264", fps=24)
