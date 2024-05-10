b1e61680cbcc8cc3e51f82ae8276922afbf19abb
vincent-el <vince198299@gmail.com>
2024-05-10 19:24:46 +0900

    audio add eleven labs

--- audio.py
+++ audio.py
@@ -2,23 +2,57 @@ import os
 from dotenv import load_dotenv
 
 from openai import OpenAI
+from elevenlabs import Voice, VoiceSettings, play
+from elevenlabs.client import ElevenLabs
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ELEVEN_API_KEY = os.getenv("ELEVEN_API_KEY")
 
-client = OpenAI()
+def generate_audio(
+    input_text: str,
+    output_path: str,
+    type: str = "openai",
+    voice_id: str = None
+):
+    if type == "openai":
+        client = OpenAI()
+        response = client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=input_text
+        )
+        response.write_to_file(output_path)
+        return response.content
+    
+    elif type == "elevenlabs":
 
-def generate_audio(input_text: str, output_path: str):
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=input_text
-    )
-    response.write_to_file(output_path)
-    return response.content
+        client = ElevenLabs(
+            api_key=ELEVEN_API_KEY
+        )
+        if voice_id is None:
+            voice_id = "5f5f1a7f-4b3e-4f5b-9f3e-9f9e2c1d0c4d" # shitty default, pls replace
+        audio = client.generate(
+            text=input_text,
+            voice=Voice(
+                voice_id=voice_id,
+                settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
+            ),
+            stream=True
+        )
+        
+        # TODO: fix the image saving
+        with open(output_path, "wb") as f:
+            f.write(audio)
+        # play(audio)
+        
+        return audio
 
 
 
 # input_text = "A sudden death, a mystery unfolds."
 # output_path = "./output/audio/1.mp3"
 # audio = generate_audio(input_text, output_path)
+
+
+
