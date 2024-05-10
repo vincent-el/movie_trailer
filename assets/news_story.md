0e3af7a850d88c318cd6e1dd31e8f25793de17fa
vincent-el <vince198299@gmail.com>
2024-05-10 18:10:01 +0900

    parameterize news_story

---
+++ assets/news_story.md
@@ -0,0 +1,38 @@
+Armed robbers convicted over drug dealer's death
+17 hours ago
+Jeremy Britton,
+BBC News
+
+Share
+Metropolitan Police Olsi Kuka Metropolitan Police
+Olsi Kuka died on 11 May
+A team of armed robbers have been convicted over the death of a drug dealer who was stabbed in his bed for his "ill-gotten gains".
+
+Olsi Kuka, 30, was attacked by a heavily armed group in the middle of the night on 11 May 2022 at his home in Whetstone, north London.
+
+After a trial at the Old Bailey, Santana Thompson, 21, and Daige Ramsey, 25, were found guilty of manslaughter.
+
+At a previous trial their accomplice Reuban Bernard was found guilty of murder after carrying out the fatal stabbing.
+
+"Ill-gotten gains"
+Prosecutor Edward Brown KC told the trial: "The evidence shows that the object of the attack was a robbery of a drug dealer - of his drugs and/or his cash, no doubt made from dealing in drugs.
+
+"The victim had been stabbed seven times and had a further three knife wounds to his body," he said.
+
+He added: "Olsi Kuka was robbed and killed for his ill-gotten gains so that the team could instead use the drugs and money.
+
+"What is plain is that this was a planned attack and each man was playing his part, knowing what was to take place and what might be needed in a robbery of a drug dealer."
+
+A number of knives and a BB gun had been carried to the scene, leaving one man dead and another injured, he said.
+
+The "organiser" of the attack, Orzan Seran, had also pleaded guilty to the manslaughter of Olsi Kuka and to unlawfully wounding his cousin, Alush Kuka.
+
+The defendants in the current trial, Santana Thompson and Daige Ramsey, had also been convicted at the earlier trial of conspiracy to rob, together with Bernard and Seran.
+
+The court heard that another defendant, Bulent Bakir from Enfield, was convicted of conspiracy to rob.
+
+The trial heard he was not inside the address like the others, but had driven one part of the team of robbers to the scene and away again and was "an essential element of the enterprise".
+
+Thompson and Ramsey, both from Waltham Forest, were also found guilty of the unlawful wounding of Alush Kuka, following the trial.
+
+Sentencing was adjourned to a future date.
--- main.py
+++ main.py
@@ -4,50 +4,9 @@ import video
 import audio
 import editor
 
-news_story = """
-Pennsylvania pastor survives attempted shooting during service
-4 days ago
-By Holly Honderich,
-in Washington
+with open('./assets/news_story.md', 'r') as file:
+    news_story = file.read()
 
-Share
-
-Contains some violence and some upsetting scenes.
-
-1:18
-Contains some violence and some upsetting scenes.
-
-Watch: The moment pastor survives shooting attempt during sermon
-A Pennsylvania pastor survived a shooting in the middle of his Sunday sermon when the attacker's gun jammed, giving congregants time to subdue him.
-
-Footage of the attack shows pastor Glenn Germany diving behind his lectern after a man approaches, raises his arm and aims a gun at Mr Germany.
-
-Police identified the gunman as 26-year-old Bernard Polite.
-
-After Mr Polite's arrest, police found another man fatally shot at his North Braddock home.
-
-The victim was identified as Derek Polite, 56. Police have not yet commented on Bernard Polite's relationship with him. Detectives from the Allegheny County Police Department's homicide unit are now handling the investigation. No charges have been filed in the shooting of the Derek Polite at this point.
-
-The Reverend Glenn Germany, pastor at Jesus' Dwelling Place Church in North Braddock, a Pittsburgh suburb, told the BBC's US partner CBS News he believed God had saved him from the attack.
-
-"I'm so grateful," he said. "You know, he shot. You can hear the gun click."
-
-Mr Germany said he saw Mr Polite walk in and smile at him, but assumed he was coming to speak with another member of the church.
-
-"But instead he came right here where I'm standing... and that's where he just pulled out the gun out," he said. "I'm looking right down the barrel of the gun."
-
-Google Maps Jesus' Dwelling Place ChurchGoogle Maps
-Jesus' Dwelling Place Church where Sunday's attack occurred
-When Mr Polite's gun failed to discharge, church Deacon Clarence McCallister ran up the centre aisle and tackled him. While Mr Polite was being restrained, Mr Germany approached and took the gun.
-
-Once police arrived, Mr Germany said Mr Polite spoke to him and apologised.
-
-Mr Polite appeared to suffer from mental illness, Mr Germany said, and told him he heard voices. "The voices were in his head and said 'go shoot the pastor.'"
-
-Mr Polite is facing multiple charges, including attempted homicide. He was denied bail and is being detained at a jail in Pittsburgh ahead of a preliminary hearing next week.
-
-According to a criminal complaint obtained by WTAE, Mr Polite told police that he tried to shoot Mr Germany because "God told him to do it" and he was hoping to go to jail to clear his mind.
-"""
 script.generate_script(news_story)
 # print(script)
 
